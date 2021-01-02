01aec1c0a65747f56aa8efb32775c7968d03e177
zhangjinglearning <zhangjinglearning@gmail.com>
2021-01-02 13:27:22 +0800

    初始化 vue 和 bulma

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an Bulma.css page</h1>
+    <div class="buttons">
+      <button class="button is-primary">Primary</button>
+      <button class="button is-link">Link</button>
+    </div>
+
+    <div class="buttons">
+      <button class="button is-info">Info</button>
+      <button class="button is-success">Success</button>
+      <button class="button is-warning">Warning</button>
+      <button class="button is-danger">Danger</button>
+    </div>
+  </div>
+</template>
